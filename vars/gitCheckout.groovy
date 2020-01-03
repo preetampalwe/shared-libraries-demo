489daba22d08e594f7543ec8b63a8c23b543e79d
Preetam Palwe <preetam.palwe@gmail.com>
2020-01-03 20:11:23 +0530

    typo fixed

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -3,7 +3,7 @@
 def call(Map stageParams)
 {
 
-    checkout([$class: 'GitSCM', branches: [[name: stageParams.branch]], userRemoteConfigs: [[credentialsId: stageParams.credId, url: stageParam.url]]])
+    checkout([$class: 'GitSCM', branches: [[name: stageParams.branch]], userRemoteConfigs: [[credentialsId: stageParams.credId, url: stageParams.url]]])
 
 }
 
